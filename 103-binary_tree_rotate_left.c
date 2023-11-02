7160bffe8da656a5efdf153470336fd30f72afaa
root <gemgbeminiyi@gmail.com>
2023-11-02 15:12:26 -0700

    updated

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,48 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - function that performs a left-rotation on a binary tree
- * @tree: pointer to the root node
- * Description: 22. Rotate left
- * Return: pointer to new root node
+ * binary_tree_rotate_left - Function that performs a left-rotation on a binary tree
+ * @tree: The root node of the three
+ * Return: Pointer node of the new node
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	/* declare and initialize pointers */
-	binary_tree_t *new = NULL, *root = NULL;
+	binary_tree_t *pivot;
 
-	/* if root node is NULL */
-	if (tree == NULL)
-		return (tree);
-
-	/* if root node has no children */
-	if ((tree->left == NULL) && (tree->right == NULL))
-		return (tree);
-
-	/* set pointer to new root */
-	new = tree->right;
-	/* set pointer to new root's left child */
-	tree->right = new->left;
-	/* if a new root's left child is valid, then */
-	/* new root will become the tree */
-	if (new->left != NULL)
-		new->left->parent = tree;
-	/* swap parents */
-	new->parent = tree->parent;
-	/* if there's no parent, current root will point to new root */
-	/* else if current root's parent has a left child, point it to new root */
-	/* all else, point current root's parent's right child to new root */
-	if (tree->parent == NULL)
-		root = new;
-	else if (tree == tree->parent->left)
-		tree->parent->left = new;
-	else
-		tree->parent->right = new;
-	/* move tree to the left of new */
-	new->left = tree;
-	tree->parent = new;
-	/* if current root is not NULL, return it, otherwise return new root*/
-	if (root)
-		return (root);
-	return (new);
+	if (tree == NULL || tree->right == NULL)
+	{
+		return (NULL);
+	}
+	pivot = tree->right;
+	tree->right = pivot->left;
+	if (pivot->left != NULL)
+	{
+		pivot->left->parent = tree;
+	}
+	pivot->left = tree;
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+	return (pivot);
 }
