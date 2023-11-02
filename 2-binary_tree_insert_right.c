3b2dcc306fdb5e1599edfac3f6ac77c687f6f45f
root <gemgbeminiyi@gmail.com>
2023-11-02 14:40:43 -0700

    advanced task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,23 +10,23 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
-	if (parent == NULL || value == '\0')
+	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	/* Allocate memory to the new node */
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
+	/* if parent already has right node */
 	if (parent->right != NULL)
 	{
+		/* old right is now new_node's right */
 		new_node->right = parent->right;
-		new_node->right->parent = new_node;
+		/* set old right's parent to new_node */
+		parent->right->parent = new_node;
 	}
-	parent->right = new_node;
+	parent->right = new_node; /* point parent's right to new node */
+
 	return (new_node);
 }
