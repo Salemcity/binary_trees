1323e6173a9e3802f3f4f98857424a652ef30119
root <gemgbeminiyi@gmail.com>
2023-11-08 13:14:52 -0800

    task update

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,26 +1,48 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right - Function that performs a right-rotation on a binary tree
- * @tree: Pointer to the root node
- * Return: Pointer to the new node
+ * binary_tree_rotate_right - right rotate binary tree
+ * @tree: pointer to the root node
+ * Description: 23. Rotate right
+ * Return: pointer to new root node
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *pivot;
+	/* declare and initialize pointers */
+	binary_tree_t *new = NULL, *root = NULL;
 
-	if (tree == NULL || tree->left == NULL)
-	{
-		return (NULL);
-	}
-	pivot = tree->left;
-	tree->left = pivot->right;
-	if (pivot->right != NULL)
-	{
-		pivot->right->parent = tree;
-	}
-	pivot->right = tree;
-	pivot->parent = tree->parent;
-	tree->parent = pivot;
-	return (pivot);
+	/* if root node is NULL */
+	if (tree == NULL)
+		return (tree);
+
+	/* if root node has no children */
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (tree);
+
+	/* set pointer to new root */
+	new = tree->left;
+	/* set pointer to new root's right child */
+	tree->left = new->right;
+	/* if a new root's right child is valid, then */
+	/* new root will become the tree */
+	if (new->right != NULL)
+		new->right->parent = tree;
+	/* swap parents */
+	new->parent = tree->parent;
+	/* if there's no parent, current root will point to new root */
+	/* else if current root's parent has a right child, point it to new root */
+	/* all else, point current root's parent's left child to new root */
+	if (tree->parent == NULL)
+		root = new;
+	else if (tree == tree->parent->right)
+		tree->parent->right = new;
+	else
+		tree->parent->left = new;
+	/* move tree to the right of new */
+	new->right = tree;
+	tree->parent = new;
+	/* if current root is not NULL, return it, otherwise return new root*/
+	if (root)
+		return (root);
+	return (new);
 }
